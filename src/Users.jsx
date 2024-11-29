402bdf1f8e8042c0355a79fc3d7a249da61cb9ef
suraj pandit <suraejpaedt@gmail.com>
2024-11-29 21:31:16 +0545

    a

--- src/Users.jsx
+++ src/Users.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 function Users() {
   return (
     <div
-    className="flex bg-blue ">Users</div>
+   className="d-flex vh-100 bg-primary justify-content-center align-items-center">Users
+   <div  className="w-50 bg-white rounded p-3">
+
+   <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+        
+            <tbody></tbody>
+            </table>
+
+   </div>
+   
+   
+   
+   </div>
   )
 }
 
